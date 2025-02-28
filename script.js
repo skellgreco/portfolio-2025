1cd6a2bd88b936531dfbf26af81193d55841c53a
skellgreco <63358722+skellgreco@users.noreply.github.com>
2025-02-28 22:10:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+    var tl = gsap.timeline();
+    gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin)
+    
+    
+
+
+
+
+   });
